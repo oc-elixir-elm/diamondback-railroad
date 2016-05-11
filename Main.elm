1f2cc3b5991c29605b80fd8227391fa988cc19ff
Scott Smith <scottnelsonsmith@gmail.com>
2016-05-10 19:41:56 -0700

    Some updates in Main.elm that will probably be changed some more

--- Main.elm
+++ Main.elm
@@ -1,18 +1,10 @@
 -- import Effects exposing (Never)
 -- import Game exposing (init, update, view)
--- import Board exposing (view)
+import Board exposing (view)
 -- import Board exposing (init, update, view)
 import Graphics.Element
-import Position
+-- import Position
 import Color exposing (..)
-
--- ONly needed when rendering Position
--- pixelsAcross = 75
-
-
-makePosition : Graphics.Element.Element
-makePosition =
-  Position.view 70 darkBrown lightBrown
 
 
 main =
