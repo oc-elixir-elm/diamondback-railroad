c54fcc1f19598b247983fdd2da2f5b96405d94f1
Scott Smith <scottnelsonsmith@gmail.com>
2016-05-13 22:01:25 -0700

    Now centers the board in the middle of the window

--- MainBoard.elm
+++ MainBoard.elm
@@ -1,5 +1,13 @@
-import Board exposing (view)
+import Graphics.Element exposing (..)
+import Window
+import Board
 
 
+main : Signal Element
 main =
-  view
+  Signal.map view Window.dimensions
+
+
+view : (Int,Int) -> Element
+view (w,h) =
+  container w h middle Board.view
