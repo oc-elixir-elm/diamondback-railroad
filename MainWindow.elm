3904fa06d86c26ca0a2b4eb99501dfb3738e3b5a
Scott Smith <scottnelsonsmith@gmail.com>
2016-05-13 22:01:04 -0700

    Added experiment to show Window dimensions

---
+++ MainWindow.elm
@@ -0,0 +1,12 @@
+import Graphics.Element exposing (..)
+import Window
+
+
+main : Signal Element
+main =
+  Signal.map view Window.dimensions
+
+
+view : (Int,Int) -> Element
+view (w,h) =
+  container w h middle (show (toString (w, h)))
