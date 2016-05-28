5d49ec4e6f6f35a3d7c738a216e42438e743bc3e
Scott Smith <scottnelsonsmith@gmail.com>
2016-05-27 18:30:44 -0700

    Almost have Board.elm compiling

--- Board.elm
+++ Board.elm
@@ -16,18 +16,66 @@ import Color exposing (Color, lightBrown, darkBrown)
 import Time exposing (Time, second)
 
 
--- UPDATE
-
-update : Msg -> Model -> Model
-update msg model =
-    model
-
--- VIEW
--- Number of positions on the side of the boafd
+-- MODEL
 
 
 type alias PosCount =
   Int
+
+
+maxPosLength : PosCount
+maxPosLength =
+  11
+
+
+positionFromInit : Matrix.Location -> Position.Model
+positionFromInit location =
+  let
+    ( position, msg ) =
+      Position.init location
+  in
+    position
+
+
+createMatrix : PosCount -> Matrix Position.Model
+createMatrix posCount =
+  Matrix.square posCount (\location -> positionFromInit location)
+
+
+type alias Model =
+  { board : Matrix Position.Model }
+
+
+init : ( Model, Cmd Msg )
+init =
+  ( { board = createMatrix maxPosLength }, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Tick Time
+
+
+update : Msg -> Model -> Model
+update msg model =
+  model
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every second Tick
+
+
+
+-- VIEW
+-- Number of positions on the side of the boafd
 
 
 type alias BoardSideInPixels =
@@ -46,17 +94,6 @@ type alias Dimensions =
   ( Width, Height )
 
 
-type alias Model =
-  (Matrix Position.Model
-   -- I'm sure something will need to be added.
-  )
-
-
-maxPosLength : PosCount
-maxPosLength =
-  11
-
-
 borderColor : Color
 borderColor =
   darkBrown
@@ -71,83 +108,15 @@ borderThickness : Int
 borderThickness =
   10
 
-createMatrix : PosCount -> BoardSideInPixels -> Matrix Html
-createMatrix posCount boardSideInPixels =
-  let
-    posSideInPixels =
-      boardSideInPixels // maxPosLength
-  in
-    -- Matrix.square posCount (\location -> Position.view posSideInPixels borderColor fillColor)
-    Matrix.square posCount ((text " square") posSideInPixels borderColor fillColor)
-
-
-{-
-smallestEdgeInPixels : Dimensions -> Int
-smallestEdgeInPixels ( x, y ) =
-  if x > y then
-    y
-  else
-    x
-
-
-makeBoardView : Matrix Element -> Element
-makeBoardView matrix =
-  let
-    rows =
-      Matrix.toList matrix
-
-    viewRows =
-      List.map (Graphics.Element.flow right) rows
-  in
-    Graphics.Element.flow down viewRows
--}
-
 
 render_rows =
   text " square"
 
-init : (Model, Cmd Msg)
-init =
-    (createMatrix maxPosLength (boardWithBorder - (2 * borderThickness)), Cmd.none)
-
-type Msg
-  = Tick Time
 
 view : Model -> Html Msg
 view model =
-{-
-  let
-    boardWithBorder =
-      smallestEdgeInPixels ( w, h )
-
-    boardMatrix =
-      createMatrix maxPosLength (boardWithBorder - (2 * borderThickness))
-
-    myBoard =
-      makeBoardView boardMatrix
-        |> container boardWithBorder boardWithBorder middle
-
-  in
-    --    color borderColor myBoard
--}
   let
     rows =
-      Matrix.toList matrix
+      Matrix.toList model.board
   in
     List.map rows render_rows
-{-
-    div []
-      [ div []
-          [ span []
-              [ text "square" ]
-          , span []
-              [ text "square" ]
-          ]
-      , div []
-          [ span []
-              [ text "square" ]
-          , span []
-              [ text "square" ]
-          ]
-      ]
--}
