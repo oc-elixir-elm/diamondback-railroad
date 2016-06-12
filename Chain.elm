0d52e74ae4ca4b70e59a395e1bbee5b39fa3ecb8
Scott Smith <scottnelsonsmith@gmail.com>
2016-06-11 21:06:52 -0700

    Merging 'Chain.elm' from hackfest today

--- Chain.elm
+++ Chain.elm
@@ -1,8 +1,42 @@
-module Chain exposing (init, view, update, subscriptions)
+module Chain exposing (Model, init, view, update, subscriptions)
 
 import Html exposing (Html)
-import Html.App
+import Piece exposing (Msg, PieceNumber)
+import AnimationFrame
+import Time exposing (Time)
 
 
 type alias Model =
-    List Piece
+    List Piece.Model
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( [], Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    AnimationFrame.times Animate
+
+
+type Msg
+    = Show
+    | Animate Time
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    let
+        ( pieceNumber, x, y ) =
+            tuple
+
+        ( piece, _ ) =
+            Piece.initWithInfo pieceNumber sideSize ( x, y )
+    in
+        piece
+
+
+view : Model -> Html Msg
+view model =
+    Html.text "chain"
