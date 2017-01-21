a521fe8b46aa2ac237101560ab27a933ec833b8e
Scott Smith <scottnelsonsmith@gmail.com>
2017-01-21 09:13:15 -0800

    Updated window to Elm 0.18

--- MainWindow.elm
+++ MainWindow.elm
@@ -1,14 +1,88 @@
-module Main exposing (..)
+module MainWindow exposing (..)
 
-import Graphics.Element exposing (..)
+import Html exposing (Html, div, text)
+import Task
 import Window
 
 
-main : Signal Element
 main =
-    Signal.map view Window.dimensions
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
-view : ( Int, Int ) -> Element
-view ( w, h ) =
-    container w h middle (show (toString ( w, h )))
+type alias Model =
+    { windowSize : Window.Size
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { windowSize =
+            Window.Size 0 0
+      }
+    , Task.perform Resize Window.size
+      -- update size for initial display
+    )
+
+
+type Msg
+    = Resize Window.Size
+    | Idle
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Resize newSize ->
+            ( { model
+                | windowSize = newSize
+              }
+            , Cmd.none
+            )
+
+        Idle ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Window.resizes Resize
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div []
+            [ text
+                ("Width: "
+                    ++ toString model.windowSize.width
+                )
+            ]
+        , div []
+            [ text
+                ("Height: "
+                    ++ toString model.windowSize.height
+                )
+            ]
+        ]
+
+
+
+--module Main exposing (..)
+--
+--import Graphics.Element exposing (..)
+--import Window
+--
+--
+--main : Signal Element
+--main =
+--    Signal.map view Window.dimensions
+--
+--
+--view : ( Int, Int ) -> Element
+--view ( w, h ) =
+--    container w h middle (show (toString ( w, h )))
